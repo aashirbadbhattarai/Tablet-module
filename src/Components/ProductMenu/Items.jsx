36bb2e11b0c3b63fc0066eb3f128ee497b161861
Aashirbad Bhattarai <aashirbadbhattarai@gmail.com>
2025-06-06 18:06:20 +0545

    UI and custom animation

--- src/Components/ProductMenu/Items.jsx
+++ src/Components/ProductMenu/Items.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import food from '../../assets/pizza.jpg'
+import './Items.css'
 
 
 const Items = ({ onClick }) => {
@@ -11,8 +12,9 @@ const Items = ({ onClick }) => {
   return (
     <div
       onClick={() => onClick && onClick(itemData)}
-      className='w-[10rem] h-[10rem] outline-1 p-2 cursor-pointer'
-    >        <img src={food} alt="Food" className='w-[60%] h-[60%] rounded-2xl' />
+      className='w-[7rem] h-[9rem] p-2 cursor-pointer product-shadow rounded-4xl'
+    >        
+        <img src={food} alt="Food" className='w-[60%] h-auto rounded-2xl' />
         <div className='flex flex-col item-center'>
             <span>Pizza</span>
             <span>Rs350.00</span>
